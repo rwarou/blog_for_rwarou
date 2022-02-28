23c23c3f73f76334971f31c40f11b88b33f827c9
Kay Jeon <kay@lever-t.com>
2022-02-28 13:03:25 +0900

    career 수정

--- src/pages/career/items/ProjectWrap.js
+++ src/pages/career/items/ProjectWrap.js
@@ -5,14 +5,19 @@ export function ProjectWrap({ career }) {
   return (
     <div className='career-content-projects-content'>
       {career.projects.map(function (project) {
+        function totalTime() {
+          const t = (parseInt(project.projectEnd.split('-')[1]) - parseInt(project.projectStart.split('-')[1])) + 1
+          return t < 0 ? t + 12 : t
+        }
+
         return (
           <div key={project.id} className='project-wrap'>
             <p>{project.name}</p>
-            <p>{convertDateDotExceptDay(project.projectStart)} - {convertDateDotExceptDay(project.projectEnd)}</p>
+            <p>{convertDateDotExceptDay(project.projectStart)} - {convertDateDotExceptDay(project.projectEnd)} ({totalTime()}개월)</p>
             <ul>
               {project.descriptions.map(function (description) {
                 return (
-                  <li key={description}>{description}</li>
+                  <li key={description}>- {description}</li>
                 )
               })}
             </ul>
